3c987904aba76ec9134220564616ea0e1084706c
emilembretsen <164795735+emilembretsen@users.noreply.github.com>
2024-04-13 13:29:55 +0200

    Add files via upload

--- P-Uppgiften-EmilEmbretsen/candidate.py
+++ P-Uppgiften-EmilEmbretsen/candidate.py
@@ -1,6 +1,11 @@
 class Candidate:
-
+    """
+    Class for the candidates with their attributes.
+    """
     def __init__(self, name, age, gender, beauty, intelligence, humor, wealth, education):
+        """
+        Initializes a Candidate object with their specified attributes.
+        """
         self.name = name
         self.age = age
         self.gender = gender
@@ -15,9 +20,14 @@ class Candidate:
         """
         Return a string representing the candidate and their attributes.
         """
-        return ("Name: " + self.name + ", Age: " + str(self.age) + ", Gender: " + self.gender +
-                ", Beauty: " + str(self.beauty) + ", Intelligence: IQ of " + str(self.intelligence) +
-                ", Humor: " + str(self.humor) + ", Wealth: " + str(self.wealth) +
-                "kr, Education: " + str(self.education))
-
-
+        return (
+            f"Name: {self.name}\n"
+            f"Grade: {self.grade}\n"
+            f"Age: {self.age}\n"
+            f"Gender: {self.gender}\n"
+            f"Beauty: {self.beauty}\n"
+            f"Intelligence: IQ of {self.intelligence}\n"
+            f"Humor: {self.humor}\n"
+            f"Wealth: {self.wealth}kr\n"
+            f"Education: {self.education}"
+        )
--- P-Uppgiften-EmilEmbretsen/program.py
+++ P-Uppgiften-EmilEmbretsen/program.py
@@ -1,43 +1,52 @@
 from candidate import Candidate
 
+
 def read_candidates_from_file(file_name):
     """
-    Reads all lines from file_name and creates a candidate for each candidate in the file. Returns a dictionary with the candidates.
+    Reads all lines from file_name and creates a candidate for each candidate in the file.
+    Returns a dictionary with the candidates.
     """
     candidates_dict = dict()
     with open(file_name, "r", encoding="utf-8") as file:
         for line in file:
             parts = line.strip().split(";")
             candidates_dict[parts[0]] = Candidate(parts[0], int(parts[1]), parts[2],
-                                        float(parts[3]), float(parts[4]), float(parts[5]),
-                                        float(parts[6]), float(parts[7]))
+                                                  float(parts[3]), int(parts[4]), float(parts[5]),
+                                                  int(parts[6]), float(parts[7]))
     return candidates_dict
+
 
 def get_form_input(prompt_string):
     """
-    The user's response is returned as an integer, where the prompt string is used to communicate what the user inputs.
+    The user's response is returned as an integer between 0 and 5,
+    where the prompt string is used to communicate what the user inputs.
     """
-    correct_input = False;
+    correct_input = False
     while not correct_input:
         try:
             answer = int(input(prompt_string).strip())
-            if answer >= 0 and answer <= 5:
+            if 0 <= answer <= 5:
                 correct_input = True
+                return answer
             else:
                 print("Input must be a whole number between 0 and 5.")
         except ValueError:
             print("Input most be a whole number.")
-    return answer
+
 
 def get_gender_input(prompt_string):
-    correct_input = False;
+    """
+    The user's response is returned as either 'M', 'F' or 'B',
+    with the prompt string communicating what the user inputs.
+    """
+    correct_input = False
     while not correct_input:
         answer = str(input(prompt_string)).upper().strip()
         if answer == "M" or answer == "F" or answer == "B":
-            correct_input = True
+            return answer
         else:
-            "Svara endast med 'M', 'F' eller 'B'."
-    return answer
+            print("Answer only with 'M', 'F' or 'B'.")
+
 
 def present_form():
     """
@@ -55,7 +64,12 @@ def present_form():
 
     return form_answers
 
+
 def remove_candidates_of_wrong_gender(form_answers, candidates_dict):
+    """
+    Removes all candidates of gender that do not adhere to the client's
+    preference.
+    """
     candidates_to_remove = []
     if form_answers[0] != "B":
         for candidate in candidates_dict:
@@ -65,20 +79,22 @@ def remove_candidates_of_wrong_gender(form_answers, candidates_dict):
         del candidates_dict[candidate]
     return candidates_dict
 
-def grade_attribute(candidate, candicates_dict, attribute):
+
+def grade_attribute(candidate, candidates_dict, attribute):
     """
     Calculates the normalized score for a specific attribute.
     """
-    candidate_attribute = getattr(candidate,attribute)
+    candidate_attribute = getattr(candidate, attribute)
     highest_score = candidate_attribute
-    for person in candicates_dict:
-        if getattr(candicates_dict[person], attribute) > highest_score:
-            highest_score = getattr(candicates_dict[person], attribute)
+    for person in candidates_dict:
+        if getattr(candidates_dict[person], attribute) > highest_score:
+            highest_score = getattr(candidates_dict[person], attribute)
     try:
         score = candidate_attribute / highest_score
     except ZeroDivisionError:
         score = 0
     return score
+
 
 def grade_candidate(candidate, candidates_dict, form_answers):
     """
@@ -90,13 +106,19 @@ def grade_candidate(candidate, candidates_dict, form_answers):
     wealth_score = grade_attribute(candidate, candidates_dict, "wealth") * form_answers[4]
     education_score = grade_attribute(candidate, candidates_dict, "education") * form_answers[5]
 
-    grade = round(beauty_score+intelligence_score+humor_score+wealth_score+education_score, 2)
+    grade = round(beauty_score + intelligence_score + humor_score + wealth_score + education_score, 2)
     return grade
 
+
 def grade_all_candidates(candidates_dict, form_answers):
+    """
+    Uses the 'grade_candidate' function to grade all candidates.
+    Returns dictionary with all candidates graded.
+    """
     for candidate in candidates_dict:
-        candidates_dict[candidate].grade = grade_candidate(candidates_dict[candidate],candidates_dict,form_answers)
+        candidates_dict[candidate].grade = grade_candidate(candidates_dict[candidate], candidates_dict, form_answers)
     return candidates_dict
+
 
 def sort_candidates(candidates_dict):
     """
@@ -111,37 +133,56 @@ def sort_candidates(candidates_dict):
 
     return converted_dict
 
+
 def present_top_10_candidates(top_10_candidates_dict):
+    """
+    Presents the 10 highest graded candidates.
+    """
     print("___________________")
     print("Here are the 10 best candidates for you!")
-
+    print()
+    rank = 1
     for candidate in top_10_candidates_dict:
-        print("Grade: " + str(top_10_candidates_dict[candidate].grade) + ". " + str(top_10_candidates_dict[candidate]))
+        print(f"{rank}.")
+        print(str(top_10_candidates_dict[candidate]))
+        print()
+        rank += 1
+
 
 def choose_candidate(top_10_candidates_dict):
+    """
+    Asks the client to choose a candidate and returns the chosen candidate.
+    """
     successful_input = False
     while not successful_input:
-        choice = input("What candidate would you like to choose?").strip()
-        try:
-            chosen_candidate = top_10_candidates_dict[choice]
-            successful_input = True
-            return chosen_candidate
-        except:
-            print("Something went wrong when you typed the name of your chosen candidate.")
+        choice = input("Enter the name of the client you'd like to choose:").strip()
+        for candidate in top_10_candidates_dict:
+            if choice.lower() == candidate.lower():
+                chosen_candidate = top_10_candidates_dict[candidate]
+                return chosen_candidate
+        if not successful_input:
+            print("The candidate name you entered is not in the list of top candidates.")
             print("Please try again.")
 
+
 def main():
+    """
+    Runs the program.
+    """
     print("Welcome to the candidate-intermediation program!")
-    print("After answering some questions about your preferences, you will be presented with a list of candidates to choose from.")
+    print("After answering some questions about your preferences, "
+          "you will be presented with a list of our top 10 candidates for you "
+          "to choose from.")
     print("___________________")
-    candidates_dict = read_candidates_from_file("candidates.txt") #reads in all the candidates
-    form_answers = present_form() #presents the form and gets form answers
-    candidates_dict = remove_candidates_of_wrong_gender(form_answers,candidates_dict) #removes all candidates of unwanted gender
-    candidates_dict = grade_all_candidates(candidates_dict,form_answers) #grades the candidates
-    top_10_candidates_dict = sort_candidates(candidates_dict) #sorts and gets the top 10 candidates in a sorted dictionary
+    candidates_dict = read_candidates_from_file("candidates.txt")
+    form_answers = present_form()
+    candidates_dict = remove_candidates_of_wrong_gender(form_answers, candidates_dict)
+    candidates_dict = grade_all_candidates(candidates_dict, form_answers)
+    top_10_candidates_dict = sort_candidates(candidates_dict)
     present_top_10_candidates(top_10_candidates_dict)
     chosen_candidate = choose_candidate(top_10_candidates_dict)
 
     print("Congratulations! You have chosen " + chosen_candidate.name + "! Great choice!")
+
 
 main()
